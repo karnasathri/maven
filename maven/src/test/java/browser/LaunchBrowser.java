ab29a8144750e6c3bf18c9f93eac69564ee6acfd
karnasathri <karnasathri05@gmail.com>
2022-03-25 02:31:47 -0700

    commit

---
+++ maven/src/test/java/browser/LaunchBrowser.java
@@ -0,0 +1,20 @@
+package browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchBrowser
+{
+    WebDriver driver; 
+	@Test
+     
+     public void setUp()
+     {
+    	 WebDriverManager.chromedriver().setup();
+    	 driver=new ChromeDriver();
+    	 driver.get("https://www.google.co.in/");
+     }
+}
